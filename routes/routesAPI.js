8959695d6704c91702ba8420879caf1d6e740e9b
Jenni <96969934+jenniwritescode@users.noreply.github.com>
2022-04-17 18:26:08 -0500

    API and HTML routes

--- routes/routesAPI.js
+++ routes/routesAPI.js
@@ -1,1 +1,41 @@
 // API routes go here
+
+const router = require('express').Router();
+
+const storeNotes = require('../public/assets/js/storeNotes');
+
+// request the existing notes
+router.get('/notes', (req, res) => {
+    storeNotes
+        .getNotes()
+        .then(notes => {
+            res.json(notes)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+// post note
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    store
+        .addNote(req.body)
+        .then(note => {
+            res.json(note)
+        })
+        .catch(err => {
+            res.status(500).json(err)
+        })
+})
+
+
+// delete note
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err))
+})
+
+module.exports = router;
--- routes/routesHTML.js
+++ routes/routesHTML.js
@@ -1,1 +1,17 @@
 // html routes go here
+
+const path = require('path');
+
+const router = require('express').Router();
+
+// sends notes to the notes.html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+// sends to the homepage if a path issue exists
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+module.exports = router;
